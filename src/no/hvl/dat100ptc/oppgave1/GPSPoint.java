5492059afb3642110cf2bcb9862b99f0a713b002
SweetLejo <heldal.leonard@gmail.com>
2022-10-07 16:15:03 +0200

    reformatting task 1

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -4,68 +4,56 @@ import no.hvl.dat100ptc.TODO;
 
 public class GPSPoint {
 
-	private int time;
-	private double latitude;
-	private double longitude;
-	private double elevation;
+    private int time;
+    private double latitude;
+    private double longitude;
+    private double elevation;
 
 
+    public GPSPoint(int time, double latitude, double longitude, double elevation) {
 
-	public GPSPoint(int time, double latitude, double longitude, double elevation) {
+        this.time = time;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.elevation = elevation;
 
-		this.time = time;
-		this.latitude = latitude;
-		this.longitude = longitude;
-		this.elevation = elevation;
+    }
 
-	}
+    // TODO - get/set metoder
+    public int getTime() {
+        return this.time;
+    }
 
-	// TODO - get/set metoder
-	public int getTime() {
-		return this.time;
-	}
+    public void setTime(int time) {
+        this.time = time;
 
-	public void setTime(int time) {
+    }
 
-		this.time = time;
+    public double getLatitude() {
+        return this.latitude;
+    }
 
-	}
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
 
-	public double getLatitude() {
+    public double getLongitude() {
+        return this.longitude;
+    }
 
-		return this.latitude;
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 
-	}
+    public double getElevation() {
+        return elevation;
+    }
 
-	public void setLatitude(double latitude) {
+    public void setElevation(double elevation) {
+        this.elevation = elevation;
+    }
 
-		this.latitude = latitude;
-
-	}
-
-	public double getLongitude() {
-
-		return this.longitude;
-
-	}
-
-	public void setLongitude(double longitude) {
-		
-		this.longitude = longitude;
-	}
-
-	public double getElevation() {
-		
-		return elevation;
-	}
-
-	public void setElevation(double elevation) {
-		
-		this.elevation = elevation;
-	}
-	
-	public String toString() {
-
-		return this.time + " (" + this.latitude + "," + this.longitude + ") " + this.elevation + "\n";
-	}
+    public String toString() {
+        return this.time + " (" + this.latitude + "," + this.longitude + ") " + this.elevation + "\n";
+    }
 }
